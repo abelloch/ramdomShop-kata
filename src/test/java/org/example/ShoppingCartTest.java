c40fc44135dfd3764dca7d9b5da4c05c6b1e5181
Abelloch <abelloch.es@gmail.com>
2024-06-24 11:38:55 +0200

    tested and refactory!

--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -1,55 +1,138 @@
 package org.example;
-
 import org.junit.jupiter.api.Test;
-
+import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 
+                                                                                            //The Gathering
+//____________________________________________________________________________________________________________________________________________________________________________________________________
 class ShoppingCartTest {
-    @Test
-    void calculatePriceForMagicCards_red() {
-        ShoppingCart shoppingCart = new ShoppingCart();
 
-        Product product = new Product(null, null, false, "red", null, "Magic: The Gathering - Lightning Bolt", null);
+//Arrange
+//@Test
+//void calculatePriceForMagicCards_red() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, null, false, "red", null, "The Gathering", null);
+//Action
+//shoppingCart.addProduct(product);
+//Expected
+//assertEquals(shoppingCart.getTotalPrice(), 3.5);
+//}
 
-        shoppingCart.addProduct(product);
+//Arrange
+//@Test
+//void calculatePriceForMagicCards_blue() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, null, false, "blue", null, "Magic: The Gathering", null);
+//Action
+//shoppingCart.addProduct(product);
+//Expected
+//assertEquals(shoppingCart.getTotalPrice(), 5.0);
+//}
 
-        assertEquals(shoppingCart.getTotalPrice(), 3.5);
 
-    }
+//Arrange
+//@Test
+//void calculatePriceForMagicCards_green() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, null, false, "green", null, "Magic: The Gathering - Maga Eternal", null);
+//action
+//shoppingCart.addProduct(product);
+//Expected
+//assertEquals(shoppingCart.getTotalPrice(), 4.40 );
+//}
 
-    @Test
-    void calculatePriceForMagicCards_blue() {
-        ShoppingCart shoppingCart = new ShoppingCart();
+//Arrange
+//@Test
+//void calculatePriceForMagicCards_black() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, null, false, "black", null, "Magic: The Gathering - Maga Eternal", null);
+//Action
+//shoppingCart.addProduct(product);
+//Expected
+//assertEquals(shoppingCart.getTotalPrice(), 6.80  );
+//}
 
-        Product product = new Product(null, null, false, "blue", null, "Magic: The Gathering - Maga Eternal", null);
+//Arrange
+//@Test
+//void calculatePriceForMagicCards_brown() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, null, false, "brown", null, "Magic: The Gathering - Maga Eternal", null);
+//Action
+//shoppingCart.addProduct(product);
+//Expeted
+//assertEquals(shoppingCart.getTotalPrice(), 2.0  );
+//}
 
-        shoppingCart.addProduct(product);
 
-        assertEquals(shoppingCart.getTotalPrice(), 5.0);
+//Arrange
+//@Test
+//void calculatePriceForMagicCards_The_Black_Lotus () {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, null, false, "BLack", null, "Magic: The Gathering - Black Lotus", null);
+//Action
+//shoppingCart.addProduct(product);
+//Expected
+//assertEquals(shoppingCart.getTotalPrice(), 40000.00  );
+//}
+                                                                                                //testing animals by types
+//__________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
-    }
+//Arrange
+//@Test
+//void calculatePriceForPetsTypes_Land() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(4, null, false, "null", null, "Perro", null);
+//Action
+//shoppingCart.addProduct(product);
+//Expected
+//assertEquals(shoppingCart.getTotalPrice(), 16.80  );
+//}
 
-    @Test
-    void calculatePriceForWine() {
-        ShoppingCart shoppingCart = new ShoppingCart();
+//Arrange
+@Test
+void calculatePriceForPetsTypes_Aquatic_and_Colors_blue() {
+ShoppingCart shoppingCart = new ShoppingCart();
+Product product = new Product(null, null, false, "blue", null, "Pez", BigDecimal.valueOf(0.1));
+//Action
+shoppingCart.addProduct(product);
+//Expected
+assertEquals(shoppingCart.getTotalPrice(), 0.1  );
+}
 
-        Product product = new Product(null, 5, false, null, null, "el tio juanillo", null);
 
-        shoppingCart.addProduct(product);
+//Arrange
+@Test
+void calculatePriceForPetsTypes_Aquatic_and_Colors_gold() {
+ShoppingCart shoppingCart = new ShoppingCart();
+Product product = new Product(null, null, false, "blue", null, "Pez", BigDecimal.valueOf(100));
+//Action
+shoppingCart.addProduct(product);
+//Expected
+assertEquals(shoppingCart.getTotalPrice(), 100  );
+}
 
-        assertEquals(shoppingCart.getTotalPrice(), 100.0);
 
-    }
 
-    @Test
-    void calculatePriceForCheese() {
-        ShoppingCart shoppingCart = new ShoppingCart();
+                                                                                                //testing the gourmet Products
+//__________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
-        Product product = new Product(null, 5, true, null, null, "French Camembert", null);
+//@Test
+//void calculatePriceForWine() {
+//ShoppingCart shoppingCart = new ShoppingCart();
+//Product product = new Product(null, 5, false, null, null, "el tio juanillo", null);
+//shoppingCart.addProduct(product);
+//assertEquals(shoppingCart.getTotalPrice(), 100.0);
+//}
 
-        shoppingCart.addProduct(product);
+//@Test
+//void calculatePriceForCheese() {
+//ShoppingCart shoppingCart = new ShoppingCart();
 
-        assertEquals(shoppingCart.getTotalPrice(), 50.0);
+//Product product = new Product(null, 5, true, null, null, "French Camembert", null);
 
-    }
+//shoppingCart.addProduct(product);
+
+//assertEquals(shoppingCart.getTotalPrice(), 50.0);
+
+// }
 }
